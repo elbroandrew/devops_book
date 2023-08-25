f275200e5877eb16ba620ca2dd7ddb30b421ff17
andrew <elbroandrew@gmail.com>
2023-08-25 21:02:49 +1000

    argparse

---
+++ c002/main.py
@@ -0,0 +1,18 @@
+#! /usr/bin/python3
+import argparse
+
+if __name__ == '__main__':
+    
+    parser = argparse.ArgumentParser(description="New CLI program")
+    parser.add_argument('message', help='Message to echo.')  # positional arg
+
+    parser.add_argument('--twice', '-t',  # - or -- means the args are optional
+                        help="Do it twice.",  
+                        action='store_true'  # make it boolean                        
+                        )
+
+    args = parser.parse_args()
+    print(args.message)
+    if args.twice:
+        print(args.message)
+
