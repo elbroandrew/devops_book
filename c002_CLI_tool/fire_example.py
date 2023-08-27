caad4f04a0fa1ea6ac7c318c7108876bb72ec96a
andrew <elbroandrew@gmail.com>
2023-08-27 21:46:12 +1000

    fire with ships example

--- c002_CLI_tool/fire_example.py
+++ c002_CLI_tool/fire_example.py
@@ -6,11 +6,11 @@ import fire
 fire package allows create CLI automatically.
 """
 
-def greet(greeting="Привет", name="Андрей"):
-  print(f"{greeting, name}")
+# def greet(greeting="Привет", name="Андрей"):
+#   print(f"{greeting, name}")
 
-if __name__ == '__main__':
-  fire.Fire(greet)
+# if __name__ == '__main__':
+#   fire.Fire(greet)
 
 """
 run:
@@ -19,3 +19,34 @@ or
 ./fire_example.py --greeting Hello --name Dron
 
 """
+
+# fire CLI tool with Class example with Ships
+
+class Ships:
+    
+    def sail(self):
+        ship_name = "Мой корабль"
+        print("%s поднял паруса" % ship_name)
+
+    def list_ships(self):
+        ships = ["Стремительный", "Перехватчик", "Жемчужина"]
+        print(f"Корабли: {', '.join(ships)}")
+
+def sailors(greeting, name):
+    print(f"{greeting} {name}")
+
+class Cli:
+    
+    def __init__(self) -> None:
+        self.sailors = sailors
+        self.Ships = Ships()
+
+if __name__ == "__main__":
+    fire.Fire(Cli)
+
+"""
+run:
+./fire_example.py Ships list-ships
+./fire_example.py sailors Hello John
+"""
+
