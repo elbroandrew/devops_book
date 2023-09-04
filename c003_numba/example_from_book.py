60d763985796eaaa0d50d5de0c6eacec2eee5008
andrew <elbroandrew@gmail.com>
2023-09-04 20:17:47 +1000

    numba jit

--- c003_numba/example_from_book.py
+++ c003_numba/example_from_book.py
@@ -7,7 +7,7 @@ def timing(f):
     @wraps(f)
     def wrap(*args, **kwargs):
         ts = time()
-        result = f(*args, **kwargs)
+        result = f()
         te = time()
         print(f"fun: {f.__name__}, args: [{args}, {kwargs}] took: {te-ts} sec")
 
@@ -17,11 +17,10 @@ def timing(f):
 
 @timing
 @numba.jit(nopython=True)
-def f(n):
-    s = 0
-    for i in range(n):
-        s += sqrt(i)
-    return s
+def f():
+    l = []
+    for i in range(150000):
+        l.insert(0, i)
 
 
 f(200)  # 2.19 sec withut jit; and 0.29 sec with jit.
