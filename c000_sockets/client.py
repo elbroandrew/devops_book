998764e287d72fa6c5e8ae651fe51fe344927e9f
Andrew <elbroandrew@gmail.com>
2023-09-13 15:54:49 +1000

    simple client TCP socket

--- c000_sockets/client.py
+++ c000_sockets/client.py
@@ -1,5 +1,13 @@
 import socket
 
 
-sock = socket()
+sock = socket.socket(    
+    family=socket.AF_INET,
+    type=socket.SOCK_STREAM,
+    proto=0,
+)
+
+sock.bind(("127.0.0.1", 5002))
+sock.connect(("127.0.0.1", 5001))
+sock.close()
 
